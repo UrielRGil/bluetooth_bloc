41a7602d8354cecb9ceab82882a8897601a24fee
UrielRoblesG <urielrobles735@gmail.com>
2023-02-08 20:58:08 -0600

    Add btn action

--- lib/widgets/result_device_tile.dart
+++ lib/widgets/result_device_tile.dart
@@ -4,19 +4,20 @@ class ResultDeviceTile extends StatelessWidget {
   final String name;
   final String address;
   final String rssi;
+  final VoidCallback? onTap;
 
   const ResultDeviceTile(
       {super.key,
       required this.name,
       required this.address,
-      required this.rssi});
+      required this.rssi,
+      this.onTap});
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(name),
       subtitle: Text(address),
-      onTap: () {},
-      onLongPress: () {},
+      onTap: onTap,
     );
   }
 }
